77ad6eeaf17c3ec93550f6c2148c6d08a20e98cd
Deivison Francisco <deivisondfs@gmail.com>
2015-12-03 11:50:36 -0300

    Commit inicial

---
+++ puppet/modules/apache/manifests/init.pp
@@ -0,0 +1,13 @@
+# -- apache install
+class apache{
+	package { "apache2":
+		ensure => installed,
+		require => Exec["apt-update"]
+	}
+
+	service { "apache2":
+		ensure => running,
+		enable => true,
+		require => Package["apache2"],
+	}
+}
---
+++ script.sh
@@ -0,0 +1,7 @@
+cd /tmp
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+dpkg -i puppetlabs-release-trusty.deb
+apt-get update
+apt-get install puppet -y
+puppet -V
+service puppet start
